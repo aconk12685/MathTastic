4001285e8c672b0e399cb2e11dee134ae7c10bd8
Andy Conk <andrew.conk@gmail.com>
2015-04-07 22:45:41 -0400

    latest changes

---
+++ MathTastic/SpaceScene.h
@@ -0,0 +1,15 @@
+//
+//  SpaceScene.h
+//
+//  Created by Andrew Conk on 10/25/14.
+//  Copyright (c) 2014 Andrew Conk. All rights reserved.
+
+#import "QuestionAnswerScene.h"
+#import "BaseScene.h"
+#import "UserGameData.h"
+
+@interface SpaceScene : BaseScene <SKPhysicsContactDelegate>
+
+@property (nonatomic, strong) SKSpriteNode *selectedNode;
+
+@end
